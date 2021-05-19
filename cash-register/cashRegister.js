8f7cbe9a16c17821fe303bb815eb1ea871f8a3f8
Andrew Huntington <andrew.huntington@gmail.com>
2021-05-19 18:57:07 +0900

    Scaffold Cash Register challenge

--- cash-register/cashRegister.js
+++ cash-register/cashRegister.js
@@ -20,18 +20,73 @@
 
 // TODO: All tests pass on FCC
 function checkCashRegister(price, cash, cid) {
-  var change;
-  return change;
+  const cashDue = cash - price; // 0.5
+  // console.log(cashDue); //TODO: REMOVE
+  let totalCID = 0;
+  const result = {
+    status: "",
+    change: [],
+  };
+
+  // Find the total cid
+  // TODO: make function
+  for (const unit of cid) {
+    totalCID += unit[1];
+  }
+  totalCID = totalCID;
+  // console.log(totalCID); //TODO: REMOVE
+
+  if (cashDue > totalCID) {
+    result.status = "INSUFFICIENT_FUNDS";
+  } else if (cashDue === totalCID) {
+    result.status = "CLOSED";
+    result.change = cid;
+  } else {
+    result.status = "OPEN";
+    // result.change =
+  }
+
+  return result;
 }
 
-checkCashRegister(19.5, 20, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
-]);
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+); // {status: "OPEN", change: [["QUARTER", 0.5]]}
+
+// console.log(
+//   checkCashRegister(19.5, 20, [
+//     ["PENNY", 0.01],
+//     ["NICKEL", 0],
+//     ["DIME", 0],
+//     ["QUARTER", 0],
+//     ["ONE", 0],
+//     ["FIVE", 0],
+//     ["TEN", 0],
+//     ["TWENTY", 0],
+//     ["ONE HUNDRED", 0],
+//   ])
+// ); // {status: "INSUFFICIENT_FUNDS", change: []}
+
+// console.log(
+//   checkCashRegister(19.5, 20, [
+//     ["PENNY", 0.5],
+//     ["NICKEL", 0],
+//     ["DIME", 0],
+//     ["QUARTER", 0],
+//     ["ONE", 0],
+//     ["FIVE", 0],
+//     ["TEN", 0],
+//     ["TWENTY", 0],
+//     ["ONE HUNDRED", 0],
+//   ])
+// ); // {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
